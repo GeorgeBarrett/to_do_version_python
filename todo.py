46607a8e8044cb0bc1601d922af070e860735c4b
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-03-08 14:34:21 +0000

    itertools imported. class GetMaxLineNumber created to count the maximum amount of todos. each todo will be on a seperate line. there is an indentation feature in this function that means that the todo items will always be aligned in todo.txt depsite their quantity. there is also a ranking feature that will display the todo items in order of rank. this functinality has not been created yet. if a todo item is not assigned a rank then it wall fall bellow the ones that do. the user can view their todo items by typing 'python3 todo.py list' into the terminal

--- todo.py
+++ todo.py
@@ -1,13 +1,30 @@
 import argparse
 import settings
+import itertools
+
+
+class GetMaxLineNumber:
+    # read line by line as we do not need to store it in memmory
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get(self):
+        with open(self.filename) as f:
+            for counter in itertools.count():
+                line = f.readline()
+                if not line:
+                    break
+        return counter
 
 class Handler:
+    
     def __init__(self):
         self.todo_file = settings.TODO_FILE
         self.done_file = settings.DONE_FILE
 
+    
     def handle(self):
-        """Interpret the first command line argument, and redirect."""
+        # Interpret the first command line argument, and redirect
         parser = argparse.ArgumentParser()
         parser.add_argument(
             "action",
@@ -19,6 +36,40 @@ class Handler:
 
         action = getattr(self, args.action)
         action()
+
+    
+    def list(self):
+        # Show all items in the todo file
+        parser = argparse.ArgumentParser()
+        parser.add_argument("action", choices=["list"])
+        parser.add_argument("filter", type=str, nargs="*")
+        args = parser.parse_args()
+
+        max_line_getter = GetMaxLineNumber(self.todo_file)
+        max_line_num = max_line_getter.get()
+        num_of_digits = len(str(max_line_num))
+        printed_items = 0
+        
+        with open(self.todo_file) as f:
+            items = f.readlines()
+
+            if len(items) == 0:
+                return
+       
+        sorted_items     = sorted((items, index+1) for index, items in enumerate(items))
+        index_unranked   = next(index for index, item in enumerate(sorted_items) if item[0][0]  != '(')
+        ranked_items     = sorted_items[0:index_unranked]
+        unranked_items   = sorted(sorted_items[index_unranked::], key = lambda x: x[1])
+        items            = ranked_items + unranked_items
+
+        for item in items:
+            if len(args.filter) == 0:
+                    print(f"{item[1]:>{num_of_digits}} {item[0]}")
+            else:
+                    if args.filter[0] in item[0]:
+                        printed_items += 1
+                        print(f"{item[1]:>{num_of_digits}} {item[0]}")
+        print(f"---\n{printed_items} item(s)")
 
 
 if __name__ == "__main__":
